06877101e0995c1ba7cf25d5d97bb75cb2d091ba
SHerlihy <steven_herlihy@yahoo.com>
2025-11-27 09:13:41 +0000

    start planning iam policies

--- planning/infrastructure_diagrams/service_flow/diagram.js
+++ planning/infrastructure_diagrams/service_flow/diagram.js
@@ -3,32 +3,68 @@ const serviceFlow = `
 
     Client <--> API
     
+    subgraph GRP[Get Role Policy]
+        subgraph GRPAllow[Allow]
+            GetIds[lambda > s3:GetObject]
+        end
+    end
+    
+    
     subgraph AWS
     subgraph global
+        subgraph LAIM[Lambda IAM]
+            subgraph GRP[Get Role Policy]
+                subgraph GRPAllow[Allow]
+                    GetIds[lambda > s3:GetObject]
+                end
+            end
+    
+            subgraph LRP[Lambda Resource Policy]
+                subgraph LRPAllow[Allow]
+                    APILambda["API > lambda:InvokeFunction"]
+                end
+            end
+
+        end
+    
+        GRP -.- List
+        GRP -.- Get
+        
+        Bucket -.-> GetIds
+        
+        
+        LRP -.- List
+        LRP -.- Get
+        LRP -.- Upload
+        LRP -.- Delete
+        
+        API -.-> APILambda
+
         API{API Gateway}
         Auth[authoriser lambda]
         
         API <--> Auth
-
-        subgraph authorised
-            List[Get all IDs]
-            Get[Get by IDs]
-            Upload[Upload single]
-            Delete[Delete by IDs]
-        end
 
         API <-- GET: / --> List
         API <-- GET: /?ids=[ids] --> Get
         API <-- POST: / --> Upload
         API <-- DELETE: /?ids=[ids] --> Delete
 
-        Bucket[S3 Bucket]
+        subgraph region
+            subgraph authorised
+                List[Get all IDs]
+                Get[Get by IDs]
+                Upload[Upload single]
+                Delete[Delete by IDs]
+            end
 
-        List <--> Bucket
-        Get <--> Bucket
-        Upload <--> Bucket
-        Delete <--> Bucket
-        
+            Bucket[S3 Bucket]
+
+            List <--> Bucket
+            Get <--> Bucket
+            Upload <--> Bucket
+            Delete <--> Bucket
+        end
     end
     end
 `
