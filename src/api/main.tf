ed3ed3fb59a2417e9d32aa2d4b5dbc002cdb4315
SHerlihy <steven_herlihy@yahoo.com>
2026-01-04 16:45:51 +0000

    remove unused dir

--- src/api/main.tf
+++
@@ -1,7 +0,0 @@
-module "lambda" {
-  source = "./lambda"
-
-  rest_api_id = aws_api_gateway_rest_api.storage.id
-  parent_id   = aws_api_gateway_rest_api.storage.root_resource_id
-  gateway_execution_arn = aws_api_gateway_rest_api.storage.execution_arn 
-}
