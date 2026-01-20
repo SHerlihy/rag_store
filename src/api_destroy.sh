80174d78737ddec1ec0e950316d76c531e044b72
SHerlihy <steven_herlihy@yahoo.com>
2026-01-20 08:46:46 +0000

    ssot api draft script

---
+++ src/api_destroy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+terraform -chdir=./api_routes destroy --auto-approve
+terraform -chdir=./api_resources destroy --auto-approve
+terraform -chdir=./dev_fakes destroy --auto-approve
+terraform -chdir=./create_objects destroy --auto-approve
--- src/api_draft.sh
+++ src/api_draft.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
 AUTH_KEY=$1
-STAGE_UID="dev"
+STAGE_UID=$2
+
+if [[ -z "$AUTH_KEY" ]]; then
+    read -p "Enter auth key: " AUTH_KEY
+fi
+
+if [[ -z "$STAGE_UID" ]]; then
+    read -p "Enter stage uid: " STAGE_UID
+fi
 
 terraform -chdir=./create_objects init
-terraform -chdir=./dev_fakes init
 terraform -chdir=./api_resources init
 terraform -chdir=./api_routes init
 
-# ./refreshers/create_dist.sh
-
-# CREATE FAKE BUCKET
-terraform -chdir=./dev_fakes apply --auto-approve
+case "$STAGE_UID" in
+    "prod")
+        ./refreshers/create_dist.sh
+    ;;
+    *)
+        terraform -chdir=./dev_fakes init
+        terraform -chdir=./dev_fakes apply --auto-approve
+    ;;
+esac
 
 # CREATE RESOURCES
-terraform -chdir=./dev_fakes output > ./api_resources/terraform.tfvars
+case "$STAGE_UID" in
+    "prod")
+        cat ./variables/production/bucket.txt > ./api_resources/terraform.tfvars
+        cat ./variables/production/source_id.txt >> ./api_resources/terraform.tfvars
+    ;;
+    *)
+        terraform -chdir=./dev_fakes output > ./api_resources/terraform.tfvars
+        echo "source_id = \"DUMMYID\"" >> ./api_resources/terraform.tfvars
+    ;;
+esac
 
 echo "auth_key = \"${AUTH_KEY}\"" >> ./api_resources/terraform.tfvars
 echo "stage_uid = \"${STAGE_UID}\"" >> ./api_resources/terraform.tfvars
-echo "source_id = \"DUMMYID\"" >> ./api_resources/terraform.tfvars
 
 cat ./variables/shared/api_id.txt >> ./api_resources/terraform.tfvars
 cat ./variables/shared/root_id.txt >> ./api_resources/terraform.tfvars
@@ -28,8 +48,16 @@ cat ./variables/shared/shared.txt >> ./api_resources/terraform.tfvars
 
 terraform -chdir=./api_resources apply
 
-# CREATE API BIND
-terraform -chdir=./dev_fakes output > ./create_objects/terraform.tfvars
+# CREATE VARIABLE OBJECTS
+case "$STAGE_UID" in
+    "prod")
+        cat ./variables/production/bucket.txt > ./create_objects/terraform.tfvars
+    ;;
+    *)
+        terraform -chdir=./dev_fakes output > ./create_objects/terraform.tfvars
+    ;;
+esac
+
 terraform -chdir=./api_resources output >> ./create_objects/terraform.tfvars
 
 cat ./variables/shared/api_id.txt >> ./create_objects/terraform.tfvars
@@ -39,7 +67,6 @@ terraform -chdir=./create_objects apply --auto-approve
 
 # CREATE API ROUTES
 terraform -chdir=./create_objects output > ./api_routes/objects.auto.tfvars
-
 terraform -chdir=./api_resources output > ./api_routes/resources.auto.tfvars
 
 terraform -chdir=./api_routes apply
