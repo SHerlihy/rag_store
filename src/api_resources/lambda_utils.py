e675e74b9db83fa789120107f6487665e448d30e
SHerlihy <steven_herlihy@yahoo.com>
2026-01-28 08:12:20 +0000

    pass single sentances

--- src/api_resources/lambda_utils.py
+++ src/api_resources/lambda_utils.py
@@ -1,6 +1,6 @@
 import re
 
-def findTerminatorIdcs(text):
+def findTerminatedIdcs(text):
     tIdcs = []
     terminators = re.compile('(?s:.)([\\.!?"])(?s:.)')
     matches = re.finditer(terminators, text)
@@ -14,30 +14,39 @@ def findTerminatorIdcs(text):
         after = text[tIdx+1]
 
         if term == '"':
-            if after == "\n":
-                tIdcs.append(tIdx)
+            if after != "\n":
+                continue
 
-        elif term == ".":
-            if after != "." and prev != ".":
-                tIdcs.append(tIdx)
+        if term == after or term == prev:
+            continue
 
-        else:
-            tIdcs.append(tIdx)
+        if after == "\n":
+            tIdx+=1
+
+        tIdcs.append(tIdx+1)
 
     return tIdcs
 
-# def splitByLineCount(text, count):
-#     splits = []
-#
-#     tIdcs = findTerminatorIdcs(text)
-#
-#     lIdx = 0
-#     rIdx = count
-#     while rIdx < len(splits):
-#         splits.append(text[lIdx:rIdx])
-#         lIdx = rIdx
-#         rIdx+=count
-#
-#     splits.append(text[lIdx:])
-#
-#     return splits
+def splitByLineCount(text, count):
+    splits = []
+
+    tIdcs = findTerminatedIdcs(text)
+
+    splits.append(text[:tIdcs[0]])
+
+    tIdxLeft = 0
+    tIdxRight = count
+    while tIdxRight < len(tIdcs):
+        sIdxLeft = tIdcs[tIdxLeft]
+        sIdxRight = tIdcs[tIdxRight]
+
+        splits.append(text[sIdxLeft:sIdxRight])
+        tIdxLeft = tIdxRight
+        tIdxRight+=count
+
+    finalTerminated = tIdcs[tIdxLeft]
+
+    if finalTerminated < len(text):
+        splits.append(text[finalTerminated:])
+
+    return splits
--- src/api_resources/lambda_utils_test.py
+++ src/api_resources/lambda_utils_test.py
@@ -1,32 +1,86 @@
 import unittest
-from lambda_utils import findTerminatorIdcs
+from lambda_utils import findTerminatedIdcs, splitByLineCount
 
-class TerminatorIdcsShould(unittest.TestCase):
+class TerminatedIdcsShould(unittest.TestCase):
     def test_return_empty_array_for_empty_string(self):
-        terminatorIdcs = findTerminatorIdcs("")
+        terminatorIdcs = findTerminatedIdcs("")
         self.assertEqual(terminatorIdcs, [])
 
     def test_return_empty_array_for_non_terminating_string(self):
-        terminatorIdcs = findTerminatorIdcs("nio[vron903u90t2niowv")
+        terminatorIdcs = findTerminatedIdcs("nio[vron903u90t2niowv")
         self.assertEqual(terminatorIdcs, [])
 
     def test_return_empty_array_for_non_terminating_terminators_string(self):
-        terminatorIdcs = findTerminatorIdcs('vow...vniw"vw"""p')
+        terminatorIdcs = findTerminatedIdcs('vow...vniw"vw"""p')
         self.assertEqual(terminatorIdcs, [])
 
-class TerminatorIdcsShouldReturnCorrectIdcs(unittest.TestCase):
+class TerminatedIdcsShouldReturnCorrectIdcs(unittest.TestCase):
     def test_for_period_string(self):
-        terminatorIdcs = findTerminatorIdcs("0123. 67. 101112.")
-        self.assertEqual(terminatorIdcs, [4,8])
+        terminatorIdcs = findTerminatedIdcs("0123. 67. 101112.")
+        self.assertEqual(terminatorIdcs, [5,9])
 
     def test_for_punctuation_string(self):
-        terminatorIdcs = findTerminatorIdcs("012? 5678! 12. 56?")
-        self.assertEqual(terminatorIdcs, [3,9,13])
+        terminatorIdcs = findTerminatedIdcs("012? 5678! 12. 56?")
+        self.assertEqual(terminatorIdcs, [4,10,14])
 
     def test_for_quotes_string(self):
-        terminatorIdcs = findTerminatorIdcs('01234 "789"\n2345')
-        self.assertEqual(terminatorIdcs, [10])
+        terminatorIdcs = findTerminatedIdcs('01234 "789"\n2345')
+        self.assertEqual(terminatorIdcs, [12])
 
     def test_for_newline_string(self):
-        terminatorIdcs = findTerminatorIdcs("012?\n5678!\n12.\n56?")
-        self.assertEqual(terminatorIdcs, [3,9,13])
+        terminatorIdcs = findTerminatedIdcs("012?\n5678!\n12.\n56?")
+        self.assertEqual(terminatorIdcs, [5,11,15])
+
+    def test_for_excess_string(self):
+        sentances = [
+            'vavvaniovwip.\n',
+            'vavvaniovwip?\n',
+            ]
+
+        multi_punctuation = "".join(sentances)
+
+        terminatorIdcs = findTerminatedIdcs(multi_punctuation)
+        self.assertEqual(terminatorIdcs, [14, 28])
+
+class SplitByLineCountShouldReturnStringArray(unittest.TestCase):
+    def test_for_single_strings(self):
+        sentances = [
+            'vavvaniovwip.\n',
+            'vavvaniovwip?\n',
+            'vavvaniovwip!\n',
+            'vavvanio "vwip"\n',
+            'vavvaniovwip!\n',
+            'vavvaniovwip?\n'
+            ]
+
+        multi_punctuation = "".join(sentances)
+
+        segments = splitByLineCount(multi_punctuation, 1)
+        
+        print(segments)
+
+        self.assertEqual(len(segments), len(sentances))
+
+        self.assertEqual(segments, sentances)
+
+    # def test_for_multi_punctuation(self):
+    #     sentances = [
+    #         'vavvaniovwip.\n',
+    #         'vavvaniovwip?\n',
+    #         'vavvaniovwip!\n',
+    #         'vavvanio "vwip"\n',
+    #         'vavvaniovwip!\n',
+    #         'vavvaniovwip?\n'
+    #         ]
+    #
+    #     multi_punctuation = "".join(sentances)
+    #
+    #     segments = splitByLineCount(multi_punctuation, 2)
+    #
+    #     self.assertEqual(len(segments), 3)
+    #
+    #     self.assertEqual(segments, [
+    #         sentances[0]+sentances[1],
+    #         sentances[2]+sentances[3],
+    #         sentances[4]+sentances[5]
+    #         ])
